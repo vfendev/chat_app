57f4a7aec22b89ee85c543812bfac30dd4d3da13
velimir <velimir.patrnogic@gmail.com>
2020-09-02 22:33:46 +0200

    first commit of chat app

---
+++ public/js/chat.js
@@ -0,0 +1,15 @@
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+const form = document.querySelector('#message-form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message)
+})
